221fc3e840252557b69cfb2151b52165d704f21d
NimraAkram321 <nimraakram321@gmail.com>
2023-01-19 12:28:43 +0500

    ASCII Add for every wrong guess

--- hangman.py
+++ hangman.py
@@ -1,13 +1,71 @@
 
 
 import random
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
+lives=6
 #  empty List  display.
+
 display=[]
 for _ in chosen_word:
     display+="_"
@@ -24,12 +82,20 @@ while not end_of_game:
         letter=chosen_word[position]
         if letter==guess:
             display[position]=letter
-    
-    print(display)
+    if guess not in chosen_word:
+        lives-=1;
+        print(stages[lives])
+        if lives==0:
+            
+            end_of_game=True
+            print("You lose")
+
+    print(f"{''.join(display)}")
+
     if "_" not in display:
         end_of_game=True
         print("You Win")
 
 
-
+    
 
